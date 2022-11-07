2d3b7d4c1ad53ddc84c8717cc38cf2a4a74d2c2b
Brandon <58617118+heyitsmass@users.noreply.github.com>
2022-11-06 23:18:19 -0800

    Moved database type into config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os 
+import json 
+import time 
 
 class MissingTokenError(Exception): 
   def __str__(self): 
@@ -37,6 +39,55 @@ class Token(str):
     return self.__token 
 
 
+class Database:
+  '''
+    Constructs and opens a database for guildID 
+
+    Ensures database integrity.  
+
+    Parameters: 
+      guildID:int -> 
+      
+        The guild for the database being requested 
+
+      mode:str = 'w' -> 
+
+        The mode the database should be opened for, a fork of open() 
+
+        
+  ''' 
+  def __init__(self, guildID:int, mode:str='w'): 
+    self.guildID = guildID 
+    self.dbLoc = f'dependencies/database/{guildID}.json'
+    if not os.path.isfile(self.dbLoc): 
+      self.dbFile = open(self.dbLoc, 'x')
+      self.db = {} 
+      self.db['emoji_links'] = {}  
+    else: 
+      self.db = json.load(open(self.dbLoc, 'r'))
+      self.mode = mode 
+      self.dbFile = open(self.dbLoc, mode) 
+
+  def __del__(self): 
+    if self.mode == 'w': 
+      json.dump(self.db, self.dbFile) 
+    
+    self.dbFile.close() 
+  
+  def __getitem__(self, key): 
+    return self.db[key]
+
+  def __setitem__(self, key, value): 
+    self.db[key] = value 
+
+  def __delitem__(self, key): 
+    del self.db[key] 
+
+  def keys(self): 
+    return self.db.keys() 
+  
+  def values(self): 
+    return self.db.values()
 
 
 
